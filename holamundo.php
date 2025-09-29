838c74d315c6dc0a9ca6c67f724fa7a8ef2c8c54
Roberto <rodriguezmartinez.roberto28@gmail.com>
2025-09-29 12:34:50 +0100

    Cambio en el holamundo

--- holamundo.php
+++ holamundo.php
@@ -1,8 +1,5 @@
 <?php
-// Este es un comentario en PHP
-<<<<<<< HEAD
-echo "¡Hola Mundo. Roberto Rodriguez Martinez";
-=======
-echo "¡Hola Mundo desde PHP!";
->>>>>>> f1d561e0b7ccab28a32c1647429f2ca5f2a36d30
+// Este es un script básico de PHP que imprime un mensaje en pantalla
+
+echo "Hola Mundo. Roberto Rodriguez Martinez";
 ?>
